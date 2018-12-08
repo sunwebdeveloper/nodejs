2e4c7095109201475d5311baea7059659151c3c1
Rafael L Santos <sunwebdeveloper@gmail.com>
2018-12-07 23:10:30 -0200

    finalizando websocket

--- DONTPAD/server.js
+++ DONTPAD/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+io.on('connection', socket =>{
+    console.log('Conectado')
+    socket.on('CLIENT_NOVAMENSAGEM',(dadoQueVeioDoClient)=>{
+        console.log('Houve uma mensagem vinda do client')
+        io.emit('SERVER_NOVAMENSAGEM',dadoQueVeioDoClient)
+    })
+})
 
 app.get('/', (req,res)=>{
     res.render('home.ejs')
 })
 
 port = 3000
-app.listen(port, () => {
+http.listen(port, () => {
     console.log(`subindo na porta ${port}`)
 })
